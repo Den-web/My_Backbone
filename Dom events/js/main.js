a8d13d955ce0ce2d753d51926e51a021816d41a4
Den-web <kirevden@gmail.com>
2016-01-31 22:30:46 +0200

    Добавление удаление записи

--- Dom events/js/main.js
+++ Dom events/js/main.js
@@ -13,21 +13,66 @@ $(function() {
 	};
 
 	App.Models.Task = Backbone.Model.extend({});
-	App.View.Task = Backbone.View.extend({
+	App.Views.Task = Backbone.View.extend({
 		tagName: 'li',
+		template: template('taskTemplate'),
 		render: function () {
-			this.$el.html( this.model.get('title') );
+			var template = this.template(this.model.toJSON());
+			this.$el.html( template );
 			return this;
+		},
+		events:{
+			'click .edit': 'editTask'
+		}
+		editTask: function(){
+			prompt('Как переименуем задачу?', this.model.get('title'));
+			this.model.set('title', newTaskTitle);
 		}
 	});
 
-	var task = new App.Models.Task({
+	App.Collections.Task = Backbone.Collection.extend({
+		model: App.Models.Task
+	});
+
+	App.Views.Tasks = Backbone.View.extend({
+		tagName: 'ul',
+		render: function() {
+			this.collection.each(this.addOne, this);
+			return this;
+		},
+		addOne: function(task) {
+			//создавать новый дочерний вид
+			var taskView = new App.Views.Task({ model: task });
+			//добавить его в дочерний елемент
+			this.$el.append(taskView.render().el);
+		}
+	})
+	/*var task = new App.Models.Task({
 		title: 'Сходить в магазин',
 		priority: 4
 
-	});
-	var taskView = new App.View.Task({ model: task });
+	});*/
 
-	console.log(taskView.render().el);
+	window.tasksCollection = new App.Collections.Task([
+	{
+		title: 'Сходить в магазин',
+		priority: 4
+	},
+	{
+		title: 'Получить почту',
+		priority: 3
+	},
+	{
+		title: 'Сходить на работу',
+		priority: 5
+	},
+	]);
+
+	var tasksView = new App.Views.Tasks({ collection: tasksCollection});
+
+	
+	$('.tasks').html(tasksView.render().el);
+
+
 
 });
